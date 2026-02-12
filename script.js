176a58b62ad61ca295668c449ec162ad5496430d
juniorkwinda71-eng <juniorkwinda71@gmail.com>
2026-02-12 10:17:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// CLEAR OLD SAVED ANSWER
-localStorage.removeItem("ratiAnswer");
-
 // message to type
 const text = "I know you said yess my baby🥺❤️ I just wanted to let you know that I love you soo much Rati!! Kego rata ka pelo yaka eyohle mbaaah❤️💋";
 
 let index = 0;
 const speed = 50;
 
-// when cover YES clicked
+// when YES clicked → show message
 function startMessage() {
     document.getElementById("coverCard").classList.add("hidden");
     document.getElementById("mainCard").classList.remove("hidden");
@@ -20,24 +17,11 @@ function typeMessage() {
         document.getElementById("message").innerHTML += text.charAt(index);
         index++;
         setTimeout(typeMessage, speed);
-    } else {
-        document.getElementById("choices").classList.remove("hidden");
-        showSavedAnswer();
     }
 }
 
-// save answer
-function selectAnswer(answer) {
-    localStorage.setItem("ratiAnswer", answer);
-    showSavedAnswer();
-}
-
-// show saved answer
-function showSavedAnswer() {
-    const saved = localStorage.getItem("ratiAnswer");
-
-    if (saved) {
-        document.getElementById("result").innerText = "She selected: " + saved;
-    }
+// when NO clicked → show text under button
+function showNo() {
+    document.getElementById("noResult").innerText = "No 💔";
 }
 
